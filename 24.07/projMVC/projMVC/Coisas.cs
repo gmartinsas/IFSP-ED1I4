d7b0d743310feafdd7bbf510338d9a5566e9fccf
Gabriel Martins <102676099+gmartinsas@users.noreply.github.com>
2023-07-31 09:48:06 -0300

    Add files via upload

---
+++ 24.07/projMVC/projMVC/Coisas.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projMVC
+{
+    internal class Coisas
+    {
+        private Coisa[] asCoisas;
+        private int max;
+        private int qtde;
+
+        public int Max { 
+            get => max; 
+        }
+        public int Qtde {
+            get => qtde; 
+        }
+        internal Coisa[] AsCoisas {
+            get => asCoisas; 
+        }
+
+        public Coisas(int max)
+        {
+            this.max = max;
+            this.qtde = 0;
+            this.asCoisas = new Coisa[this.max];
+            for(int i= 0; i < this.max; i++)
+            {
+                this.asCoisas[i] = new Coisa(-1, "...");
+            }
+        }
+
+        public string mostrar()
+        {
+            string s = "";
+            foreach(Coisa c in this.asCoisas)
+            {
+                if (c.Id != -1)
+                {
+                    s += c.ToString();
+                }
+            }
+            return s;
+        }
+
+        public bool adicionar(Coisa c)
+        {
+            bool podeAdicionar = (this.qtde < this.max);
+            if (podeAdicionar)
+            {
+                this.asCoisas[this.qtde++] = c;
+            }
+            return podeAdicionar;
+        }
+
+        public Coisa pesquisar(Coisa c)
+        {
+            Coisa coisaAchada = new Coisa(-1, "...");
+            int i = 0;
+            while (i < this.max && this.asCoisas[i].Id != c.Id)
+            {
+                i++;
+            }
+            if (i<this.max)
+            {
+                coisaAchada = this.asCoisas[i];
+            }
+            return coisaAchada;
+        }
+
+        public bool remover(Coisa c)
+        {
+            _ = new Coisa(-1, "...");
+            Coisa coisaPesquisada = pesquisar(c);
+            for (int i = 0; i < this.asCoisas.Length; i++)
+            {
+                if (coisaPesquisada == this.asCoisas[i])
+                {
+                    this.asCoisas[i].Id = -1;
+                    this.asCoisas[i].Descricao = "...";
+                }
+            }
+            return true;
+        }
+    }
+
+
+}
---
+++ 24.07/projMVC/projMVC/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projMVC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Coisas minhasCoisas = new Coisas(5);
+            Console.WriteLine((minhasCoisas.adicionar(new Coisa(1, "Coisa 1"))) ? "Adicionou":"Não adicionou");
+            Console.WriteLine((minhasCoisas.adicionar(new Coisa(2, "Coisa 2"))) ? "Adicionou" : "Não adicionou");
+            Console.WriteLine((minhasCoisas.adicionar(new Coisa(3, "Coisa 3"))) ? "Adicionou" : "Não adicionou");
+            Console.WriteLine((minhasCoisas.adicionar(new Coisa(4, "Coisa 4"))) ? "Adicionou" : "Não adicionou");
+            Console.WriteLine((minhasCoisas.adicionar(new Coisa(5, "Coisa 5"))) ? "Adicionou" : "Não adicionou");
+            Console.WriteLine((minhasCoisas.adicionar(new Coisa(6, "Coisa 6"))) ? "Adicionou" : "Não adicionou");
+            Console.WriteLine(minhasCoisas.mostrar());
+
+            Coisa coisaPesquisada = minhasCoisas.pesquisar(new Coisa(3));
+            if (coisaPesquisada.Id != -1)
+            {
+                Console.WriteLine(coisaPesquisada.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Coisa não encontrada");
+            }
+            Console.WriteLine(minhasCoisas.pesquisar(new Coisa(2)).ToString());
+
+            // Código inserido para a atividade
+            Console.WriteLine(minhasCoisas.remover(new Coisa(2)) ? "Removeu" : "Não removeu");
+            Console.WriteLine(minhasCoisas.mostrar());
+        }
+    }
+}
